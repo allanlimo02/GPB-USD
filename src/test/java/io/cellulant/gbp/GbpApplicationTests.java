9088d0c3af2fb9de244e1a742839c1beeaae90d9
allanlimo02 <allanlimo02@gmail.com>
2023-02-08 10:57:13 +0300

    initial commit

---
+++ src/test/java/io/cellulant/gbp/GbpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cellulant.gbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
